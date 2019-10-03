eb7e6ea1d1fa0188e76f1fbb812d445ac5c3d190
EugeneBykovsky <bykovsky.ssau@gmail.com>
2019-10-03 21:51:13 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,91 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+  let arr = expression.split('*');
+  let c0 = 0;
+  let c2 = 0;
+  let c5 = 0;
+
+  arr.forEach((item) => {
+    if (item.indexOf('!!') != -1) {
+      item = item.replace('!!', '');
+      if (item % 2 === 0) {
+        for (let j = 0; j <= item; j++) {
+          if (j % 10 === 0 & j != 0) {
+            let tens = String(j).split('');
+            for (let k = 0; k < tens.length; k++) {
+              if (tens[k] === '0') {
+                c0 += 1;
+              } else if (tens[k] === '5') {
+                c5 += 1;
+              } else if (tens[k] === '2') {
+                c2 += 1;
+              }
+            }
+          } else if (j % 2 === 0 & j != 0) {
+            c2 += 1;
+          } else if (j % 5 === 0 & j != 0) {
+            if (j % 25 === 0) {
+              c5 += 2;
+            } else {
+              c5 += 1;
+            }
+          }
+          j += 1;
+        }
+      } else {
+        for (let j = 1; j <= item; j++) {
+          if (j % 10 === 0 & j != 0) {
+            let tens = String(j).split('');
+            for (let k = 0; k < tens.length; k++) {
+              if (tens[k] === '0') {
+                c0 += 1;
+              } else if (tens[k] === '5') {
+                c5 += 1;
+              } else if (tens[k] === '2') {
+                c2 += 1;
+              }
+            }
+          } else if (j % 2 === 0) {
+            c2 += 1;
+          } else if (j % 5 === 0 & j != 0) {
+            if (j % 25 === 0) {
+              c5 += 2;
+            } else {
+              c5 += 1;
+            }
+          }
+          j += 1;
+        }
+      }
+    } else if (item.indexOf('!') != -1) {
+      item = item.replace('!', '');
+      for (let j = 1; j <= item; j++) {
+        if (j % 10 === 0 & j != 0) {
+          let tens = String(j).split('');
+          for (let k = 0; k < tens.length; k++) {
+            if (tens[k] === '0') {
+              c0 += 1;
+            } else if (tens[k] === '5') {
+              c5 += 1;
+            } else if (tens[k] === '2') {
+              c2 += 1;
+            }
+          }
+        } else if (j % 2 === 0) {
+          c2 += 1;
+        } else if (j % 5 === 0 & j != 0) {
+          if (j % 25 === 0) {
+            c5 += 2;
+          } else {
+            c5 += 1;
+          }
+        }
+      }
+    }
+  });
+
+  if (c2 < c5) {
+    return (c2 + c0);
+  } else {
+    return (c5 + c0);
+  }
+};
